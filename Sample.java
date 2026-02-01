3d83e92a43ee2b67c8c1a30223507c0f9bedf481
prajwalbodkhe928@gmail.com <116057965+prajwal-55@users.noreply.github.com>
2026-02-01 15:57:29 +0530

    adding implementation for Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,6 @@
 public class Sample {
     public static void main(String[] args){
-        int a=6;
-        int b=7;
-        System.out.println(a*b);
+
+        System.out.println("Jay Maharashtra");
     }
 }
